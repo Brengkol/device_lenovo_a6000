d5413199faf0f96100a3c8184212c2fae0ca7e92
Ekorudianto27 <Layeardteam@gmail.com>
2020-12-18 13:27:22 +0000

    Nad-a6000

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_a6000.mk
+    $(LOCAL_DIR)/nad_a6000.mk
 
 # Lunch choices
 COMMON_LUNCH_CHOICES := \
-    ancient_a6000-userdebug
+    nad_a6000-userdebug
