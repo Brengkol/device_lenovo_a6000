bad3c90ebb0b65b4a62f0c69c37a35b5259f9f75
Ekorudianto27 <Layeardteam@gmail.com>
2020-12-22 13:28:46 +0000

    a6000: use havoc-bringeup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_a6000.mk
+    $(LOCAL_DIR)/havoc_a6000.mk
 
 # Lunch choices
 COMMON_LUNCH_CHOICES := \
-    aosp_a6000-userdebug
+    havoc_a6000-userdebug
