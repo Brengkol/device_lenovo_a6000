4de7328ba2463061a629ee77f1fb91cbbbb8b1d8
Ekorudianto27 <Layeardteam@gmail.com>
2020-12-22 14:46:05 +0000

    havoc

--- havoc_a6000.mk
+++ havoc_a6000.mk
@@ -16,10 +16,9 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/lenovo/a6000/device.mk)
-$(call inherit-product, vendor/havoc/config/common.mk)
 
-# Aosp Only
-TARGET_BOOT_ANIMATION_RES := 720
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
