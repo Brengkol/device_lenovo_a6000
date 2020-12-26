fcb0d3e4595f8164e564bb4424b18ebdc04ca0b6
Ekorudianto27 <Layeardteam@gmail.com>
2020-12-26 16:21:54 +0000

    a6000: stagOs bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_a6000.mk
+    $(LOCAL_DIR)/stag_a6000.mk
 
 # Lunch choices
 COMMON_LUNCH_CHOICES := \
-    ancient_a6000-userdebug
+    stag_a6000-userdebug
