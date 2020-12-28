0dbfd4f6e364df9d190fe6b2765ae1bec725424a
Ekorudianto27 <Layeardteam@gmail.com>
2020-12-28 16:04:05 +0000

    a6000: RR-bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a6000.mk \
     $(LOCAL_DIR)/rr_a6000.mk
 
 # Lunch choices
 COMMON_LUNCH_CHOICES := \
-    lineage_a6000-userdebug \
     rr_a6000-userdebug
