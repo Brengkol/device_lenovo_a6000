bed788645c018a144c029926c27df2750043a7a7
Ekorudianto27 <Layeardteam@gmail.com>
2020-12-30 13:25:56 +0000

    Arrow-Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,8 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_a6000.mk
+    $(LOCAL_DIR)/ancient_a6000.mk \
+    $(LOCAL_DIR)/arrow_a6000.mk
 
 # Lunch choices
 COMMON_LUNCH_CHOICES := \
-    ancient_a6000-userdebug
+    ancient_a6000-userdebug \
+    arrow_a6000-userdebug
