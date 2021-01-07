cdd7ae15a28cd1e02d83cb6fb1a9f4857ba516fe
Ekorudianto27 <Layeardteam@gmail.com>
2021-01-07 12:43:18 +0000

    a6000: trap

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_a6000.mk
+    $(LOCAL_DIR)/nad_a6000.mk
 
 # Lunch choices
 COMMON_LUNCH_CHOICES := \
-    aosp_a6000-userdebug
+    nad_a6000-userdebug
