484038cdc19a26f138a5f980fd80f5e486b23c92
Ekorudianto27 <Layeardteam@gmail.com>
2021-01-13 00:03:00 +0000

    fluid-bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a6000.mk
+    $(LOCAL_DIR)/fluid_a6000.mk
 
 # Lunch choices
 COMMON_LUNCH_CHOICES := \
-    lineage_a6000-userdebug
+    fluid_a6000-userdebug
